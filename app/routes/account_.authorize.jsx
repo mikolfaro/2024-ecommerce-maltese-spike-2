0d5f38704fe3e99a651a5fa375ecc3c45432caa2
Mikol Faro <mikol.faro@gmail.com>
2024-02-09 17:26:36 +0100

    Comment out not-needed code

--- app/routes/account_.authorize.jsx
+++ app/routes/account_.authorize.jsx
@@ -5,20 +5,19 @@ import {AppSession} from '~/lib/session';
  * @param {LoaderFunctionArgs}
  */
 export async function loader({context, request}) {
-  const session = await AppSession.init(request, [context.env.SESSION_SECRET])
-  const requestUrl = new URL(request.url)
-  const origin =
-    requestUrl.protocol === 'http:'
-      ? requestUrl.origin.replace('http', 'https')
-      : requestUrl.origin;
+  // const session = context.session
+  // const requestUrl = new URL(request.url)
+  // const origin =
+  //   requestUrl.protocol === 'http:'
+  //     ? requestUrl.origin.replace('http', 'https')
+  //     : requestUrl.origin;
+  //
+  // const code = requestUrl.searchParams.get('code')
+  // // const state = requestUrl.searchParams.get('state')
+  // const codeVerifier = session.get('customerAccount',)?.codeVerifier
+  // const clientId = context.env.PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID
+  // const redirectUri = origin + '/account/authorize';
 
-  const code = requestUrl.searchParams.get('code')
-  // const state = requestUrl.searchParams.get('state')
-  const codeVerifier = session.get('customerAccount',)?.codeVerifier
-  const clientId = context.env.PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID
-  const redirectUri = origin + '/account/authorize';
-
-  return context.customerAccount.authorize();
   // return json({
   //   grant_type: "authorization_code",
   //   client_id: clientId,
@@ -27,6 +26,8 @@ export async function loader({context, request}) {
   //   code_verifier: codeVerifier,
   //   origin: origin,
   // })
+
+  return context.customerAccount.authorize();
 }
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
